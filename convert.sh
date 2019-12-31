c97f56d956212fbb5d2eb6a39545245f0be9cd62
Sheldon <s2maschmeyer@gmail.com>
2019-12-31 17:58:13 -0500

    updated resumes

--- convert.sh
+++ convert.sh
@@ -3,3 +3,6 @@ echo "Converting Resume to .PDF, .DOCX and .TXT"
 soffice --headless --convert-to docx primaryResume.fodt
 soffice --headless --convert-to txt primaryResume.fodt
 soffice --headless --convert-to pdf primaryResume.fodt
+soffice --headless --convert-to docx summaryResume.fodt
+soffice --headless --convert-to txt summaryResume.fodt
+soffice --headless --convert-to pdf summaryResume.fodt
