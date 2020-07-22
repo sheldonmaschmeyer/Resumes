25c1cd723d8c5b3604e09e2d5bae219aeb23d756
Sheldon <s2maschmeyer@gmail.com>
2020-07-22 16:42:43 -0400

    convert now makes the Ottawa resume the master, just changes address for Toronto version

--- convert.sh
+++ convert.sh
@@ -3,6 +3,8 @@ echo "Converting Resume to .PDF, .DOCX and .TXT"
 soffice --headless --convert-to docx primaryResumeOttawa.fodt
 soffice --headless --convert-to txt primaryResumeOttawa.fodt
 soffice --headless --convert-to pdf primaryResumeOttawa.fodt
+cp primaryResumeOttawa.fodt primaryResumeToronto.fodt
+sed -i -e "s/562 Reardon Private, Ottawa, Ontario/4060 Farrier Court, Mississauga, Ontario/g" primaryResumeToronto.fodt
 soffice --headless --convert-to docx primaryResumeToronto.fodt
 soffice --headless --convert-to txt primaryResumeToronto.fodt
 soffice --headless --convert-to pdf primaryResumeToronto.fodt
