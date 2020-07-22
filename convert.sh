19caf79618727fad40928b4c5c441bc432980aff
Sheldon <s2maschmeyer@gmail.com>
2020-07-22 16:21:53 -0400

    Toronto and Ottawa resumes

--- convert.sh
+++ convert.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Converting Resume to .PDF, .DOCX and .TXT"
-soffice --headless --convert-to docx primaryResume.fodt
-soffice --headless --convert-to txt primaryResume.fodt
-soffice --headless --convert-to pdf primaryResume.fodt
+soffice --headless --convert-to docx primaryResumeOttawa.fodt
+soffice --headless --convert-to txt primaryResumeOttawa.fodt
+soffice --headless --convert-to pdf primaryResumeOttawa.fodt
+soffice --headless --convert-to docx primaryResumeToronto.fodt
+soffice --headless --convert-to txt primaryResumeToronto.fodt
+soffice --headless --convert-to pdf primaryResumeToronto.fodt
 soffice --headless --convert-to docx summaryResume.fodt
 soffice --headless --convert-to txt summaryResume.fodt
 soffice --headless --convert-to pdf summaryResume.fodt
