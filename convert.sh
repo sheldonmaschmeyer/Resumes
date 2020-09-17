931aa54a1367ad9dcdfe8ea139e9b823541d298b
Sheldon <s2maschmeyer@gmail.com>
2020-09-17 12:29:26 -0400

    update address, convert for Toronto & ended employment as IT & Media Arts ...

--- convert.sh
+++ convert.sh
@@ -4,7 +4,7 @@ soffice --headless --convert-to docx primaryResumeOttawa.fodt
 soffice --headless --convert-to txt primaryResumeOttawa.fodt
 soffice --headless --convert-to pdf primaryResumeOttawa.fodt
 cp primaryResumeOttawa.fodt primaryResumeToronto.fodt
-sed -i -e "s/562 Reardon Private, Ottawa, Ontario/4060 Farrier Court, Mississauga, Ontario/g" primaryResumeToronto.fodt
+sed -i -e "s/740 Springland Dr., Apt. 515, Ottawa, Ontario/4060 Farrier Court, Mississauga, Ontario/g" primaryResumeToronto.fodt
 soffice --headless --convert-to docx primaryResumeToronto.fodt
 soffice --headless --convert-to txt primaryResumeToronto.fodt
 soffice --headless --convert-to pdf primaryResumeToronto.fodt
